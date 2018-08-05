d36bf61e977a34879977296ff9a158dcf0621229
BELKATI samir <s_belkati@esi.dz>
2018-08-05 14:55:10 +0200

    premier commit

---
+++ src/main/java/org/engaeo/Application01Application.java
@@ -0,0 +1,12 @@
+package org.engaeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application01Application.class, args);
+	}
+}
